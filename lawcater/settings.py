64b2116411431d1c84d6d4f062200d1f8cf253b1
nisha <nishapoudel400@gmail.com>
2022-11-28 17:36:16 +0545

    commit

--- lawcater/settings.py
+++ lawcater/settings.py
@@ -47,7 +47,7 @@ INSTALLED_APPS = [
     'lawcaterapp',
 ]
 INSTALLED_APPS += ['django_social_share']
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
 
 
 MIDDLEWARE = [
@@ -137,3 +137,6 @@ MEDIA_ROOT= os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
